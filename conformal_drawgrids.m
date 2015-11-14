fdb087ab470b1c47021fa80067f577a59d95a8ad
Yingjie <1275963@gmail.com>
2015-11-14 09:24:43 +0800

    Functionlized

--- conformal_drawgrids.m
+++ conformal_drawgrids.m
@@ -1,18 +1,23 @@
-clear
-th = (0:5:60)*pi/180;
-r = 0:0.05:1;
-[TH,R] = meshgrid(th,r);
-[X,Y] = pol2cart(TH,R);
+function fi = conformal_drawgrids(x,y,fun)
 
+% clears
+% th = (0:5:60)*pi/180;
+% r = 0:0.05:1;
+% x = -0.5*pi:0.1*pi:0.5*pi;
+% y = -2:0.1:2;
+% [TH,R] = meshgrid(th,r);
+% [X,Y] = pol2cart(TH,R);
 
+fi = figure();
+[X,Y] = meshgrid(x,y);
 
-for x = 1:length(th)
-    Z = X(:,x)+1i*Y(:,x);
-    tz = sin(Z).^3;
+for xi = 1:length(x)
+    Z = X(:,xi)+1i*Y(:,xi);
+    tz = fun(Z);
     
     subplot(2,1,1)
     hold on;
-    plot(X(:,x),Y(:,x),'r')
+    plot(X(:,xi),Y(:,xi),'r')
     subplot(2,1,2)
     hold on;
     axis off
@@ -20,17 +25,18 @@ for x = 1:length(th)
 end
 
 
-for y = 1:length(r)
-    Z = X(y,:)+1i*Y(y,:);
-    tz = sin(Z).^3;
+for yi = 1:length(y)
+    Z = X(yi,:)+1i*Y(yi,:);
+    tz = fun(Z);
     
     subplot(2,1,1)
     hold on;
-    plot(X(y,:),Y(y,:),'b')
+    plot(X(yi,:),Y(yi,:),'b')
     subplot(2,1,2)
     hold on;
     plot(real(tz),imag(tz),'b');
 end
-    
+   
+end
     
     
